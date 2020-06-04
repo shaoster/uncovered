a92b4254829b3e23054bf9280f929e9f8a8c8a56
Philip Shao <shaoster@gmail.com>
2020-06-04 16:12:41 -0400

    Try koa-sslify with the xForwardedProtoResolver

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 import { Server } from 'boardgame.io/server';
 import path from 'path';
 import serveStatic from 'koa-static';
+import sslify, { xForwardedProtoResolver } from 'koa-sslify';
 import Game from './game';
 
 const server = Server({ games: [Game] });
 const PORT = process.env.PORT || 8000;
 
+// The client uses localStorage rather than cookies, which has the
+// security feature of not sharing keys between http and https urls.
+// Thus, we'll force clients to use SSL.
+// Note: Heroku uses the xForwardedProto header to indicate the protocol
+// of the original request before ssl-termination at the LB.
+if (process.env.NODE_ENV === 'production') {
+  server.app.use(sslify({ resolver: xForwardedProtoResolver}));
+}
+
 // Build path relative to the server.js file
 const frontEndAppBuildPath = path.resolve(__dirname, '../build/');
-server.app.use(async (ctx) => {
-  console.log(ctx.headers);
-  return await serveStatic(frontEndAppBuildPath)(ctx);
-});
-
+server.app.use(serveStatic(frontEndAppBuildPath));
 server.run(PORT);
