5831c3106f1091affbd173a18df667507f500224
Philip Shao <shaoster@gmail.com>
2020-06-04 14:52:49 -0400

    Write a custom handler to log stuff.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import { Server } from 'boardgame.io/server';
 import path from 'path';
-import serve from 'koa-static';
+import serveStatic from 'koa-static';
 import Game from './game';
 
 const server = Server({ games: [Game] });
@@ -8,17 +8,9 @@ const PORT = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
 const frontEndAppBuildPath = path.resolve(__dirname, '../build/');
-console.log(frontEndAppBuildPath);
-server.app.use(serve(frontEndAppBuildPath))
-
-server.run(PORT, () => {
-  server.app.use(
-    async (ctx, next) => {
-      console.log(ctx.headers);
-      await serve(frontEndAppBuildPath)(
-        Object.assign(ctx, { path: 'index.html' }),
-        next
-      );
-    }
-  );
+server.app.use(async (ctx) => {
+  console.log(ctx.headers);
+  return await serveStatic(frontEndAppBuildPath)(ctx);
 });
+
+server.run(PORT);
